20d90b96a6cfbe2c6ec134cdc80c583beed3ace7
omerfarukkaya590 <85821993+omerfarukkaya590@users.noreply.github.com>
2021-11-18 23:48:09 +0300

    Add files via upload

---
+++ proglama system/konsol programlama/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace konsol_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + " " + surname);
+        }
+    }
+}
